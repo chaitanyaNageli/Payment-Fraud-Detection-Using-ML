898197b53e8581575c92957e3835b27675010825
Chaitanya Kumar <chaitanyakumarnageli@gmail.com>
2026-02-17 20:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,5 +32,9 @@ def predict():
 
     return render_template("index.html", prediction_text=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
