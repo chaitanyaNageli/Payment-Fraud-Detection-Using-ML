7339898623359a0ad19d8e3782c33e2239310b2c
Chaitanya Kumar <chaitanyakumarnageli@gmail.com>
2026-02-18 12:46:42 +0530

    Update app.py

--- fraud-detection/app.py
+++ fraud-detection/app.py
@@ -32,5 +32,6 @@ def predict():
 
     return render_template("index.html", prediction_text=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
